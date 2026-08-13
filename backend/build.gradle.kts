d7e16f087bbe222373c7e9a84ace6374567e10ab
Erick <erick@example.com>
2026-08-13 19:07:21 +0300

    Fix Gradle dependency-management plugin compatibility issue

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     kotlin("plugin.spring") version "1.9.22"
     kotlin("plugin.jpa") version "1.9.22"
     id("org.springframework.boot") version "3.2.0"
-    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "com.ridenorth"
@@ -17,8 +16,6 @@ repositories {
     mavenCentral()
     maven { url = uri("https://repo.spring.io/milestone") }
 }
-
-extra["springCloudVersion"] = "2023.0.1"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -54,12 +51,6 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
-}
-
-dependencyManagement {
-    imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-    }
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
